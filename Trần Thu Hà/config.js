b92ac7b8f5cd4c575a3ae1d96dbce0efab0b793f
Nguyễn Đắc Hùng <hung2k4tk@gmail.com>
2021-11-02 14:59:12 +0700

    Add files via upload

---
+++ Trần Thu Hà/config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hello ! 🖐🖐🖐',
+    introDesc: `Mình có điều này muốn hỏi Hà, Hà phải trả lời thật lòng nhé`,
+    btnIntro: 'OK',
+    title: 'Hà biết mình thích Hà mà, thế Hà có thể chấp nhận làm crush của mình không ? 😙',
+    desc: 'Nếu không trả lời là đồng ý làm crush mình đó  :> ',
+    btnYes: 'Bình thường 💖💖💖',
+    btnNo: '💔 Không thích ! 💔 ',
+    question:'Mình biết Hà thấy mình phiền lắm, đáng ghét lắm vì từ lần gặp đầu tiên hồi lớp 10 mình đã để lại cho Hà ấn tượng xấu về mình mất rồi. Giá như hồi đó mình biết suy nghĩ chín chắn hơn, biết cân nhắc những hành động, những dòng tin nhắn thì đã không để Hà ghét. Mình đã nhắn tin một cách vồ vập khiến cho Hà cảm thấy thật phiền phức. Mình đã từng ước giá như khi mới vào lớp mình không nhìn thấy và không thích Hà thì tốt biết bao. Hôm đầu tiên vào lớp 10 mình đã bị thích Hà mất rồi, lớp có 17 bạn nữ nhưng hôm đó mình chỉ nhìn thấy Hà mà thôi không phải là Hà mặc đẹp cũng không phải là Hà trang điểm xinh nhất lớp mà do sự dễ thương và hồn nhiên của Hà. Giá như thời gian có thể quay trở lại thì mình sẽ không để Hà phải ghét mình vì mình phiền nữa rồi. Mình biết có nhiều bạn nam cũng thích Hà, trong 792 ngày qua nhiều lúc mình đã cố để không làm phiền đến Hà nữa nhưng mình vẫn không thể làm được. Cho dù quyết định của Hà là thế nào thì mình vẫn luôn thích Hà ma thôi ! 😊😊',
+    reasonPlaceholder: 'Lý do không thích mình',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Love 😊😊',
+    mess: 'Mình biết mà, Mình cũng thích Hà lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho mình nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: ''
+}
